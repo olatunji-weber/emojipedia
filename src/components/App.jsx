290633af61862cb87030f37e3cb0014399a09bbd
olatunji-weber <olatunji.weber@gmail.com>
2023-01-29 13:02:51 +0200

    Modified the App.jsx files using arrow functions...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,23 +2,19 @@ import React from "react";
 import Entry from "./Entry.jsx"
 import emojis from "./emojiLib"
 
-function createEntry(emoji){
-  return(
-    <Entry
-       key = {emoji.id}
-       emoji = {emoji.emoji}
-       term = {emoji.term}
-       definition = {emoji.definition}
-    />
-  );
-}
-
 function App() {
   return (
   <div>
     <h1><span>emojipedia</span></h1>  
     <dl className="dictionary">
-      {emojis.map(createEntry)};
+      {emojis.map(
+        emoji => <Entry 
+            key = {emoji.id}
+            emoji = {emoji.emoji}
+            term = {emoji.term}
+            definition = {emoji.definition.substring(0, 100)+"......"} 
+          />
+      )};
     </dl>
   </div>);
 }
